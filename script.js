64955c2b589b4e1e072c4e5ecd7948ef00bf1995
xovest <79008917+xovest@users.noreply.github.com>
2021-11-26 21:05:12 +0200

    got the schema

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const mongoose = require('mongoose');
+const User = require('./User');
 
 mongoose.connect('mongodb://localhost/testdb');
+
+run();
+async function run() {
+  const user = await User.create({
+    name: 'Ryan', 
+    age: 19,
+    hobbies: ['Gaming', 'Programming'],
+    address: {
+      street: 'Musatini 23'
+    }
+  });
+  user.name = 'Bob';
+  console.log(user);
+}
