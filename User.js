c8b51300a8196e812fe94448fe34d9b1329a2e94
xovest <79008917+xovest@users.noreply.github.com>
2021-11-26 21:18:35 +0200

    comments bru

--- User.js
+++ User.js
@@ -1,16 +1,43 @@
 const mongoose = require('mongoose');
 
+//basically the same as puttin the objects down there, but it's a detailed and separated way of doing it
 const addressSchema = new mongoose.Schema({
   street: String,
   city: String
 });
 
+//required: if you don't put it in script, it won't work
+//lower/uppercase = that's how it will show if you say that
+//default = it will automatically add it no matter what
+//immutable = you can't change what's in the script
+//min or max something is gonna show error if rule broken
+//that validate property is cool, says the error if the validator's rule is broken
 const userSchema = new mongoose.Schema({
   name: String,
-  age: Number,
-  email: String,
-  createdAt: Date,
-  updatedAt: Date,
+  age: {
+    type: Number,
+    min: 1,
+    max: 100,
+    validate: {
+      validator: v => v % 2 === 0,
+      message: props => `${props.value} is not an even number`
+    }
+  },
+  email: {
+    type: String,
+    minLength: 10,
+    required: true,
+    lowercase: true
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => Date.now()
+  },
+  updatedAt: {
+    type: Date,
+    default: () => Date.now()
+  },
   bestFriend: mongoose.SchemaTypes.ObjectId,
   hobbies: [String],
   address: addressSchema
--- script.js
+++ script.js
@@ -4,15 +4,22 @@ const User = require('./User');
 mongoose.connect('mongodb://localhost/testdb');
 
 run();
+//create method to make the user
+//save method guess what for the db
 async function run() {
-  const user = await User.create({
-    name: 'Ryan', 
-    age: 19,
-    hobbies: ['Gaming', 'Programming'],
-    address: {
-      street: 'Musatini 23'
-    }
-  });
-  user.name = 'Bob';
-  console.log(user);
+  try {
+    const user = await User.create({
+      name: 'Ryan', 
+      age: 19,
+      email: 'olarumonica2016@gmail.com',
+      hobbies: ['Gaming', 'Programming'],
+      address: {
+        street: 'Musatini 23'
+      }
+    });
+    user.name = 'Bob';
+    console.log(user);
+  } catch (e) {
+    console.log(e.message);
+  }
 }
